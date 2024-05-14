229abdfc3db3dcc556212d5e8d9b3558a68a6318
DevSeaShell <michelle.c.bolme@gmail.com>
2024-05-14 12:31:32 +0200

    php changes

--- Conndb/connection.php
+++ Conndb/connection.php
@@ -7,9 +7,15 @@ $dbName = "UserDatabase";
 
 $conn = mysqli_connect( $dbServername , $dbUsername , $dbPassword , $dbName );
 
+// Checking for connection errors
 if ($conn->connect_error)
 {
 	die("Connection failed: " . $conn->connect_error);
+}
+
+if ($result = mysqli_query($conn, "SELECT * FROM Users")){
+	echo "Returned rows are: " . mysqli_num_rows($result);
+	mysqli_free_result($result);
 }
 
 echo "EYYY"
