8823fcb06b24e9fec02f7ea8629ac311f0cc4656
DevSeaShell <152975387+DevSeaShell@users.noreply.github.com>
2024-05-15 01:34:11 +0200

    Update connection.php

--- Conndb/connection.php
+++ Conndb/connection.php
@@ -12,19 +12,6 @@ if ($conn->connect_error)
 {
 	die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected!";
-
-$mail = $conn->real_escape_string($_POST['$regmail']);
-$username = $conn->real_escape_string($_POST['$regusername']);
-$password = $conn->real_escape_string($_POST['$regpassword']);
-
-$sql = "INSERT INTO User (mail, username, passworde) VALUES ('$mail', '$username', '$password');";
-
-if ($conn->query($sql) === TRUE){
-	echo "Inserted!";
-} else {
-	echo "error:" . $sql. "<br>" .$conn->error;
-}
 
 echo "EYY";
 $conn->close();
