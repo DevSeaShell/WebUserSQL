6c7d18502e4b0ebb30cb02ffaefaab45ac089ab2
DevSeaShell <michelle.c.bolme@gmail.com>
2024-05-14 11:23:40 +0200

    Added folders, Edited URLs, Possible fix to SQLconnection

--- Conndb/connection.php
+++ Conndb/connection.php
@@ -7,5 +7,10 @@ $dbName = "UserDatabase";
 
 $conn = mysqli_connect( $dbServername , $dbUsername , $dbPassword , $dbName );
 
+if ($conn->connect_error)
+{
+	die("Connection failed: " . $conn->connect_error);
+}
+
 echo "EYYY"
 ?>
