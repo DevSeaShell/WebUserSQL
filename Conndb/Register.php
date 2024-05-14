849ba80ea9d4f14732d54ecf4cc28085fa21baef
DevSeaShell <152975387+DevSeaShell@users.noreply.github.com>
2024-05-15 00:35:24 +0200

    AAA

--- Conndb/Register.php
+++ Conndb/Register.php
@@ -12,16 +12,16 @@ if ($conn->connect_error)
 {
 	die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected!";
+echo "Connected";
 
-$mail = $conn->real_escape_string($_POST['regmail']);
+$mail = $conn->real_escape_string($_POST['regemail']);
 $username = $conn->real_escape_string($_POST['regusername']);
 $password = $conn->real_escape_string($_POST['regpassword']);
 
 $sql = "INSERT INTO users (mail, username, password) VALUES ('$mail', '$username', '$password');";
 
 if ($conn->query($sql) === TRUE){
-	echo "Inserted!";
+	echo "Inserted";
 } else {
 	echo "error:" . $sql. "<br>" .$conn->error;
 }
