1c0eb0ff0d52042fa052113140a145bf5c7239da
DevSeaShell <152975387+DevSeaShell@users.noreply.github.com>
2024-05-15 10:25:41 +0200

    o.o

--- Conndb/Register.php
+++ Conndb/Register.php
@@ -6,27 +6,30 @@ $dbName = "UserDatabase";
 
 $conn = new mysqli($servername, $username, $password, $database);
 
-
-
+// Checking for connection errors
 if ($conn->connect_error)
 {
 	die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected";
+echo "Connected!";
 
 $mail = $conn->real_escape_string($_POST['regemail']);
 $username = $conn->real_escape_string($_POST['regusername']);
 $password = $conn->real_escape_string($_POST['regpassword']);
 
-$sql = "INSERT INTO users (mail, username, password) VALUES ('$mail', '$username', '$password');";
+$sql = "SELECT * FROM users WHERE username = '$regusername'";
+	if (mysqli_num_rows($result) >=1) {
+		echo "Name already exists";
+	} else {
+		// Insert cuery here
+	$sql = "INSERT INTO users (mail, username, password) VALUES ('$mail', '$username', '$password');";
+	
+	if ($conn->query($sql) === TRUE){
+		echo "Inserted";
+	} else {
+		echo "error:" . $sql. "<br>" .$conn->error;
+	}
+	}
 
-if ($conn->query($sql) === TRUE){
-	echo "Inserted";
-} else {
-	echo "error:" . $sql. "<br>" .$conn->error;
-}
-
-echo "EYY";
-$conn->close();
+echo "EYYY"
 ?>
-
