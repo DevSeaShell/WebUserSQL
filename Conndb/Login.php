e242bdd15fd43efa68e79c3b3e8cba204b38684a
DevSeaShell <michelle.c.bolme@gmail.com>
2024-05-27 08:53:52 +0200

    Changes

--- Conndb/Login.php
+++ Conndb/Login.php
@@ -19,17 +19,19 @@ $mail = $conn->real_escape_string($_POST['logmail']);
 $username = $conn->real_escape_string($_POST['logusername']);
 $password = $conn->real_escape_string($_POST['logpassword']);
 
-// Checks if username that was written in input exists in the database.
-$sql_get = "SELECT * FROM users WHERE username = '$username' AND password = '$password' AND mail = '$mail'";
+// Checks if input exists in the database.
+$sql_get = "SELECT * FROM users WHERE username = '$username'";
 $result = $conn -> query($sql_get);
 echo "Database Result";
 //Code above works fine ------------
 
-$sql = "SELECT id, mail, password FROM users WHERE username = ?"
+if ($sql = "SELECT id, mail, password FROM users WHERE username = ?") {
 $stmt = $conn -> Prepare($sql);
 $stmt -> bind_param('s', $username);
 $stmt -> execute();
 $stmt -> store_result();
+echo "RESULTS!!";
+}
 
 //if ($stmt num_rows = 1){
 	//$stmt -> bind_result($id, $mail, $password);
