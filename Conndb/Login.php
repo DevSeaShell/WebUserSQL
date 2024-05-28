bae2a77ed3ffbb1e04e8ef8c134d76654b564f35
DevSeaShell <michelle.c.bolme@gmail.com>
2024-05-28 13:24:19 +0200

    Fixed annoying typo ;-;

--- Conndb/Login.php
+++ Conndb/Login.php
@@ -20,7 +20,7 @@ $username = $conn->real_escape_string($_POST['logusername']);
 $password = $conn->real_escape_string($_POST['logpassword']);
 
 print_r($mail);
-print_r($_POST['logpassword']);
+print_r($password);
 // Checks if input exists in the database.
 $sql_get = "SELECT * FROM users WHERE username = '$username'";
 $result = $conn -> query($sql_get);
