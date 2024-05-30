d66b8d115562296c3c3ec77bd07f1729802ca563
DevSeaShell <michelle.c.bolme@gmail.com>
2024-05-30 08:37:59 +0200

    ;-;

--- Pages/home.php
+++ Pages/home.php
@@ -15,7 +15,7 @@ if (!isset($_SESSION['id'])){
     <title>Home</title>
 </head>
 <body>
-    <h1>Welcome! <?=htmlspecialchars($_SESSION['username']);?> You are logged in!</h1>
+    <h1>Welcome! <?php htmlspecialchars($_SESSION['username']);?> You are logged in!</h1>
 
     <div id = HeaderPages>
         <a href = "home.php"> Home</a>
