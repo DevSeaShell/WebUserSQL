d22c00a21c04cb803d6ec8a1ab427973051f3763
DevSeaShell <michelle.c.bolme@gmail.com>
2024-05-30 10:17:35 +0200

    added page bar

--- Pages/Profile.php
+++ Pages/Profile.php
@@ -15,5 +15,12 @@ if (!isset($_SESSION['user_username'])){
 </head>
 <body>
     <h1> Your Profile <?=htmlspecialchars($_SESSION['user_username'], ENT_QUOTES)?></h1>
+    <div id = HeaderPages>
+        <a href = "home.php"> Home</a>
+        <a href = "Profile.php"> Profile</a>
+        <a href = "home.php"> Content</a>
+        <a href = "home.php"> About us</a>
+        <a href = "home.php"> Fun games</a>
+    </div>
 </body>
 </html>
