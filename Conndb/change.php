484d51b840b2d5de0b2eeffddbe178074b44f3f8
DevSeaShell <michelle.c.bolme@gmail.com>
2024-06-11 12:54:16 +0200

    Code

--- Conndb/change.php
+++ Conndb/change.php
@@ -20,6 +20,21 @@ $CurrentUsername = $_SESSION['user_username'];
 $username = $conn->real_escape_string($_POST['newusername']);
 
 // Update usernamee
+$sql = "SELECT * FROM users WHERE username = '$username'";
+$result_u = $conn -> query($sql);
+
+if (mysqli_num_rows($result_u) > 0){
+	echo"username taken";
+} else {
+	$sql_new = "UPDATE users SET username = '$username' WHERE username ='$CurrentUsername'";
+
+	if ($conn -> query($sql_new) === TRUE){
+		$_SESSION['user_username'] = $username;
+		header("Location: ../Pages/Profile.php")
+	} else {
+		echo"Not succsessfull";
+	}
+}
 
 $conn -> close();
 
