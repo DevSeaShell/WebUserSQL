d0fb210b1f68d50744280df0293f5281cc5b356d
DevSeaShell <152975387+DevSeaShell@users.noreply.github.com>
2024-06-11 08:35:15 +0200

    Update change.php

--- Conndb/change.php
+++ Conndb/change.php
@@ -20,22 +20,21 @@ $CurrentUsername = $_SESSION['user_username'];
 $username = $conn->real_escape_string($_POST['newusername']);
 
 $sql = "SELECT * FROM users WHERE username = '$username'";
-$result_u = $conn -> query($sql);
+result_u = $conn -> query($sql);
 
 if (mysqli_num_rows($result_u) > 0){
     echo"Username Taken";
 } else {
-    $sql_new = "UPDATE users SET username = '$username' WHERE username = '$CurrentUsername'";
+    $sql_new = "UPDATE users SET username = '$usernmae' WHERE username = '$CurrentUsername'";
 
     if ($conn -> query($sql_new) === TRUE){
-        echo"Succsessfully Updated";
-        $_SESSION['user_username'] = $username;
+        echo"Succsessfully updated";
+        $_SESSION['user_usernmae'] = $username;
         header("Location: ../Pages/Profile.php");
     } else {
-        echo"Failed to Update";
+        echo"Failed to update";
     }
 }
-
 $conn -> close();
 
 echo"EYY! <br>";
